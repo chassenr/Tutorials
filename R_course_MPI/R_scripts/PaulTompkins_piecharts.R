d4e253d05c1c882cb8eeff02e455027a40b00c6a
Christiane Hassenrück <chassenr@mpi-bremen.de>
2016-09-12 18:32:35 +0200

    rudimentary R code for pie charts on map

--- R_course_MPI/R_scripts/PaulTompkins_piecharts.R
+++ R_course_MPI/R_scripts/PaulTompkins_piecharts.R
@@ -1,7 +1,6 @@
 # add pie charts at specific coordinates
 pie.coord <- ENV.all[, c("Longitute", "Latitude")]
-
-# use phylum taxonomy
+all.equal(rownames(pie.coord), rownames(seagrass))
 
 # use bioconductor R package
 source("https://bioconductor.org/biocLite.R")
@@ -10,3 +9,19 @@ biocLite("Rgraphviz")
 # load R package
 require(Rgraphviz)
 
+# open new graphics device
+plot.new()
+
+# plot coastlines
+
+# add pie charts
+for(i in 1:nrow(pie.coord)){
+  pieGlyph(
+    seagrass[i, ],
+    pie.coord[i, 2],
+    pie.coord[i, 1], 
+    labels = colnames(seagrass), 
+    col = rainbow(ncol(seagrass)), 
+    cex = 0.3
+  )
+}
